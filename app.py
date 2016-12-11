fc2821edcbefe0374d7d4d31def36a8eb8da2d74
NanamiTakayama <ns.potable@gmail.com>
2016-12-12 02:35:51 +0900

    seted GPIO 12 as 18

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.setup(12, GPIO.OUT)
 while True:
-    GPIO.output(11, True)
+    GPIO.output(12, True)
+    print("ON")
     time.sleep(2)
-    GPIO.output(11, False)
+    GPIO.output(12, False)
+    print("OFF")
     time.sleep(2)
