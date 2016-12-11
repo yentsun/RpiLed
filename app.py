5bf97ff7a107119c2cd2f4c5f3c84f74325a7a94
NanamiTakayama <ns.potable@gmail.com>
2016-12-12 02:06:26 +0900

    5

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(14, GPIO.OUT)
 while True:
-    GPIO.output(2, True)
+    GPIO.output(14, True)
     print("on")
     time.sleep(2)
-    GPIO.output(2, False)
+    GPIO.output(14, False)
     time.sleep(2)
     print("off")
