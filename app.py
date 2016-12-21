0514916a63918225915af07bf6171239fd18d8ce
NanamiTakayama <ns.potable@gmail.com>
2016-12-21 17:17:18 +0900

    122103

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
+#GPIO.setup(12, GPIO.OUT)
+#GPIO.setup(11, GPIO.OUT)
 while True:
     #GPIO.output(12, True)
     #GPIO.output(11, True)
